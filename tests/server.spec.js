21181e769eb8fb0cc50b690d6ecb0403c67a97cd
Vicente Hernández <vhernandezr96@gmail.com>
2024-04-02 15:35:05 -0300

    First commit

---
+++ tests/server.spec.js
@@ -0,0 +1,40 @@
+const request = require("supertest");
+const server = require("../index");
+
+describe("Operando CRUD de cafés", () => {
+  it("Obteniendo un código", async () => {
+    const response = await request(server).get("/cafes").send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+
+  it("Eliminar café con ID inexistente", async () => {
+    const jwt = "token";
+    const deleteId = 123123;
+    const response = await request(server)
+      .delete(`/cafes/${deleteId}`)
+      .set("Authorization", jwt)
+      .send();
+
+    expect(response.statusCode).toBe(404);
+  });
+
+  it("Agregar un café correctamente (201)", async () => {
+    const id = 5;
+    const cafe = { id, nombre: "Frappuccino" };
+    const response = await request(server).post("/cafes/").send(cafe);
+
+    expect(response.statusCode).toBe(201);
+  });
+
+  it("Actualizar ID incongruente", async () => {
+    const updateId = 2;
+    const id = 1;
+    const cafe = { id, nombre: "Cortado" };
+    const response = await request(server).put(`/cafes/${updateId}`).send(cafe);
+
+    expect(response.statusCode).toBe(400);
+  });
+});
